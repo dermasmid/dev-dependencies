546ebf721d1b1a57cfaf55da021e6e384fcf6569
Cheskel Twersky <cheskel@linnovate.net>
2021-10-03 12:08:49 +0300

    update install.sh add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Run
+sudo bash ./install.sh
+
+# List of app
+* `anydesk` - Remote Desktop
+* `chrome`
+* `vs code`
+* `mongodb_compass` - Gui for MongoDb
+* `rocketchat`
+* `slack`
+* `zoom`
+* `filezilla` - FTP client
+* `docker`
+* `flameshot` - For making screenshots
+* `docker_compose`
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install debs/*.deb
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install ./debs/*.deb --allow-downgrades -y
 sudo apt install filezilla python3-pip htop docker.io network-manager-openconnect-gnome flameshot -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt autoremove -y
 
-
-pip3 install docker-compose
+sudo pip3 install --trusted-host pypi.org --trusted-host files.pythonhosted.org docker-compose
 sudo chmod 666 /var/run/docker.sock
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
